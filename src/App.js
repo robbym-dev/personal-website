e2f13b43e14d69aff690ff86ab87eec98de07d42
robbym-dev <manihani@stanford.edu>
2023-12-20 00:00:26 -0700

    Converted relative path to absolute

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import Home from './Home'
 
-const App = () => { /* Adding this note to make sure Github Push works */
+const App = () => { 
 
   return (
     <div className="app">
