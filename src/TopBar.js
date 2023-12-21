5a5c1f480fbc73870f49375271b315d9ef686f37
robbym-dev <manihani@stanford.edu>
2023-12-20 23:28:57 -0700

    Style top bar

--- src/TopBar.js
+++ src/TopBar.js
@@ -1,15 +1,32 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './TopBar.css';
 
 const TopBar = () => {
+  const [scrolled, setScrolled] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const isScrolled = window.scrollY > 0;
+      setScrolled(isScrolled);
+    };
+
+    // Add event listener
+    window.addEventListener('scroll', handleScroll);
+
+    // Clean up the event listener
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
   return (
-    <nav className="nav">
-      <div src="./name-logo.png" alt="Logo" className="logo"></div> 
+    <nav className={`nav ${scrolled ? 'scrolled' : ''}`}>
+      <div className="logo"></div>
       <ul>
-        <li><a href="/">Home</a></li>
-        <li><a href="/about">About</a></li>
-        <li><a href="/services">Services</a></li>
-        <li><a href="/contact">Contact</a></li>
+        <li><a href="/">Introduction</a></li>
+        <li><a href="/services">Projects</a></li>
+        <li><a href="/contact">Experience</a></li>
+        <li><a href="/contact" className="lets-talk-btn">Let's Talk</a></li>
       </ul>
     </nav>
   );
