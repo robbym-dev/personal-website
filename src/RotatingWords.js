a84a839747088e76ef74fd310b90a2294c61f3a7
robbym-dev <manihani@stanford.edu>
2023-12-26 18:17:11 -0700

    Completed "A little bit about me" me section

--- src/RotatingWords.js
+++ src/RotatingWords.js
@@ -1,7 +1,6 @@
-import React, { useState, useEffect, useRef  } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import Particles from "react-tsparticles";
 import { loadFull } from "tsparticles";
-
 
 import './RotatingWords.css';
 
@@ -32,24 +31,24 @@ const RotatingWords = () => {
   }, []);
 
   return (
-    <div> 
+    <div>
 
       {/* Particles */}
       <div className="particles-container">
-      <Particles 
-        id="tsparticles"
-        options={{
-          /* particles configuration */
-        }}
-        style={{
-          position: 'absolute',
-          top: 0,
-          left: 0,
-          width: '100%',
-          height: '100%',
-          zIndex: -1
-        }}
-      />
+        <Particles
+          id="tsparticles"
+          options={{
+            /* particles configuration */
+          }}
+          style={{
+            position: 'absolute',
+            top: 0,
+            left: 0,
+            width: '100%',
+            height: '100%',
+            zIndex: -1
+          }}
+        />
       </div>
       {/* Landing page info */}
       <div className="outer-headings">
@@ -73,19 +72,55 @@ const RotatingWords = () => {
 
       {/* About Me Section */}
       <div className="background-image-container">
-                {/* Content that goes on top of the image, if any */}
+        {/* Content that goes on top of the image, if any */}
       </div>
       <div ref={aboutMeRef} className="about-me-section">
         <h2 className={startTyping ? 'typing-effect' : ''}>A little bit about <span className="highlight">me</span></h2>
         <p className="about-me-text">
-        Charting a path less traveled, I find myself at the intersection of technology and innovation as a <br /> Computer Science student at Stanford University. 
-        My academic journey is fueled by a deep fascination <br />  with Brain-Computer Interfaces (BCIs), Natural Language Processing (NLP), and 
-        Robotics.
-        Particularly, I'm drawn<br />  to the dynamic world of startups, where ideas transform into impactful realities. Beyond the academic sphere, I've  <br /> found a deep interest in music production and dance, specifically
-        I'm an enthusiast of culinary adventures,  <br />
-        always eager to explore new restaurants. Above all, I cherish the joy of meeting new people and exchanging ideas,  <br /> believing that every encounter is a 
-        doorway to a world of knowledge and perspectives.
+          {/* Charting a path less traveled, I find myself at the intersection of technology and innovation as a <br /> Computer Science student at Stanford University.
+          My academic journey is fueled by a deep fascination <br />  with Brain-Computer Interfaces (BCIs), Natural Language Processing (NLP), and 
+          Robotics.
+          Particularly, I'm drawn<br />  to the dynamic world of startups, where ideas transform into impactful realities. Beyond the academic sphere, I've  <br /> found a deep interest in music production and dance, specifically Bhangra.
+          I'm an enthusiast of culinary adventures,  <br />
+          always eager to explore new restaurants. Above all, I cherish the joy of meeting new people and exchanging ideas,  <br /> believing that every encounter is a 
+          doorway to a world of knowledge and perspectives. */}
         </p>
+        {/* Three Boxes */}
+        <div className="boxes-container">
+          <div className="box box-1 flip-vertical">
+            <div className="box-inner">
+              <div className="box-front">
+                <h3>Bio</h3>
+              </div>
+              <div className="box-back">
+                <p>Charting a path less traveled, I find myself at the intersection of technology and innovation as a <br /> Computer Science student at Stanford University.
+          My academic journey is fueled by a deep fascination with Brain-Computer Interfaces (BCIs), Natural Language Processing (NLP), and 
+          Robotics. Particularly, I'm drawn<br />  to the dynamic world of startups, where ideas transform into impactful realities.</p>
+              </div>
+            </div>
+          </div>
+          <div className="box box-2 flip-vertical">
+            <div className="box-inner">
+              <div className="box-front">
+                <h3>Skills</h3>
+              </div>
+              <div className="box-back">
+                <p>Content for Box 2</p>
+              </div>
+            </div>
+          </div>
+          <div className="box box-3 flip-vertical">
+            <div className="box-inner">
+              <div className="box-front">
+                <h3>Interests</h3>
+              </div>
+              <div className="box-back">
+                <p>Content for Box 3</p>
+              </div>
+            </div>
+          </div>
+        </div>
+        {/* End of Three Boxes */}
       </div>
       {/* About Me Section */}
     </div>
